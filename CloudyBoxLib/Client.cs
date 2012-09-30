68dc8d65685baaefbc68074cc1ce1db577529c32
Prashant Cholachagudda <pvc@outlook.com>
2012-09-30 23:40:47 +0530

    Added the unit tests project

--- CloudyBoxLib/Client.cs
+++ CloudyBoxLib/Client.cs
@@ -1,36 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using CloudyBoxLib.OAuth;
-using Windows.Foundation;
 
 namespace CloudyBoxLib
 {
-    public sealed class Client
+    public sealed class Client : IDisposable
     {
         private readonly string _apikey = "ud1mygzz55xaory";
         private readonly string _appSecret = "xhr7bp2ohcs541r";
-        private const string BaseUrl = "https://api.dropbox.com";
+        private const string BaseUrl = "https://api.dropbox.com/1/";
         private const string ContentBaseUrl = "https://api-content.dropbox.com";
-        private const string SignatureMethod = "PLAINTEXT";
 
         private readonly HttpClient _client;
 
         public Client()
         {
-            _client = new HttpClient(new OAuthMessageHandler(new HttpClientHandler()));
-            _client.BaseAddress = new Uri(BaseUrl);
+            _client = new HttpClient(new OAuthMessageHandler(new HttpClientHandler()))
+                          {
+                              BaseAddress = new Uri(BaseUrl)
+                          };  
         }
 
 
         public async Task<string> CreateTokenRequest()
         {
-            var s = await _client.GetAsync("1/oauth/request_token");
-            s.EnsureSuccessStatusCode();
-            return await s.Content.ReadAsStringAsync();
+            var response = await _client.GetAsync("oauth/request_token");
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
         }
     }
 }
---
+++ CloudyBoxLibTests/ClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudyBoxLib;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace CloudyBoxLibTests
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public async Task When_Requested_For_The_Token_Then_Return_Token_And_Secret()
+        {
+            using (var client = new Client())
+            {
+                string token = await client.CreateTokenRequest();
+                Debug.WriteLine(token);
+                Assert.IsTrue(!string.IsNullOrEmpty(token));
+            }
+        }
+    }
+}
